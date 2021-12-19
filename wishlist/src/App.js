42594525ec611c9e8a2e03987c818eec9f92f16e
shailesh <shailaja24305@gmail.com>
2021-12-19 12:29:03 +0530

    wishlist

---
+++ wishlist/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Shipping from './Components/Ship'
+// import Payment from './Components/Payment'
+
+function App() {
+  return (
+    <div className="App">
+     <Shipping/>
+     {/* <Payment/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ wishlist/src/Components/Wishlist.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import "./wishlist.css";
+
+function Wishlist() {
+  return (
+    <div>
+      <div id="wishlist-container">
+        <div className="wish-content">
+          <div className="wishlist-main">
+            {/* -------------------left section----------------------- */}
+            {/* ---------upper-part---------- */}
+            <div className="wishlist-left">
+              <div className="user-details">
+                <span>S</span>
+                <p>Shailaja24305@gmail.com</p>
+              </div>
+              {/* ------upper part ends------------ */}
+
+              {/* -----------bottom-list------------ */}
+              <div className="user-info">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-user-circle"></i>
+                      My Profile
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-shopping-bag"></i>
+                      My Orders
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="userActive">
+                      <i class="far fa-heart"></i>
+                      My Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-wallet"></i>My Credits
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-gift"></i>My Vouchers
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="far fa-address-book"></i>My Addresses
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-headset"></i>Contact Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-sign-out-alt"></i>Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* -----------------------------right-container---------------------------------- */}
+            <div class="right-container">
+              <div class="right-heading">
+                <h3>My Wishlist</h3>
+              </div>
+              <div class="wish-mainbox">
+                <span>Products</span>
+                <div class="wish-box">
+                  <span>Price</span> <span>Size</span> <span>Bag</span>
+                </div>
+              </div>
+              <div class="wish-product-list">
+                <div class="wish-product-name">
+                  <figure>
+                    <a href="/dusty-pink-embellished-neck-blouson-dress-83/prdt">
+                      {" "}
+                      <img
+                        src="https://img.faballey.com/images/Product/DRS04577Z/1.jpg"
+                        alt=""
+                      />
+                    </a>
+                    <p>Dusty Pink Embellished Neck Blouson Dress</p>
+                  </figure>
+                </div>
+                <div class="wish-product-right">
+                  <span>
+                    <div class="wish-price">
+                      <i class="fas fa-rupee-sign"></i> 1100
+                    </div>
+                  </span>
+
+                  <span>
+                    <div class="wish-size">
+                      <select class="size">
+                        <option>Select Size</option>
+                        <option> S </option>
+                        <option> M </option>
+                        <option> L </option>
+                        <option> XS </option>
+                        <option> XL </option>
+                      </select>
+                      <p class="error">Select Size</p>
+                    </div>
+                  </span>
+
+                  <span>
+                    <div class="wish-btn addtobag">
+                      <a class="addToCart">ADD TO BAG</a>
+                      <i class="fas fa-times"></i>
+                    </div>
+                  </span>
+                </div>
+              </div>
+
+              {/* -------------bottom btn------------------ */}
+
+              <div class="wish-addall">
+                <a href="#" class="addToAllCart">
+                  Add all to Bag
+                </a>
+              </div>
+              {/* ------------------------------------- */}
+              {/* ---------------back to home-------------- */}
+
+              <div class="wish-to-home"><a href="#">❮ BACK TO HOME</a></div>
+              {/* ------------------------------------------ */}
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Wishlist;
