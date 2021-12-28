9fb350632e175f82c7e2d04664f901622823f338
MUKUNDA <mukundakamatham@gmail.com>
2021-12-28 06:26:49 +0530

    updating navbar

--- src/Navbars/Navbar1.jsx
+++ src/Navbars/Navbar1.jsx
@@ -4,9 +4,14 @@ import Heart from "./images/heart.svg";
 import Bag from "./images/bag.svg";
 import { NavLink } from "react-router-dom";
 import "./navbar1.css";
+import { Logout } from "../Redux/auth/action";
 
-
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 function Navbar1(){
+  const dispatch = useDispatch();
+  const logout=()=>{
+dispatch(Logout)
+  }
     return(
         <div id="nav1-container">
           
@@ -24,10 +29,25 @@ function Navbar1(){
              <span> Track Order | </span>
              <span>Gift Card | </span>
              <span> User Name  | </span>
-             
-              <span id ="heartimage"> <img src={Heart}></img>  </span>
+             <div class="dropdown">
+    <button class="dropbtn">Account info
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <p ><NavLink to ="/login">Log In</NavLink></p>
+      <p ><NavLink to ="/register">Sign In</NavLink></p>
+      <p  onClick={logout}><NavLink to ="/" >Log Out</NavLink></p>
+    </div>
+  </div> 
+
+
+
+
+
+
+              <span id ="heartimage"> <img src={Heart} alt="heart"></img>  </span>
              <span> | </span>
-             <NavLink to ="/bag"><span id="bagimage"  >  <img src={Bag}></img></span></NavLink>
+             <NavLink to ="/bag"><span id="bagimage"  >  <img src={Bag} alt="nag"></img></span></NavLink>
              
             
             
@@ -38,3 +58,7 @@ function Navbar1(){
 }
 
 export {Navbar1}
+
+
+
+
